61bb71098c3c57cf0e611d5f3f32ab83cf0e9ce2
florianborter <flo.borter@gmail.com>
2018-02-15 14:11:32 +0100

    AB04 und AB05

--- 411/AB411-04/src/AB04Main.java
+++ 411/AB411-04/src/AB04Main.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class AB04Main {
+    public static void main(String[] args) {
+        int[] liste = new int[100000];
+        int[] klon1 = new int[100000];
+        int[] klon2 = new int[100000];
+        for(int i = 0; i < liste.length; i++) {
+            liste[i] = (int) (100*Math.random());
+        }
+
+        Long start = System.currentTimeMillis();
+        for(int i = 0; i < liste.length; i++) {
+            klon1[i] = liste[i];
+        }
+        Long end = System.currentTimeMillis();
+        Long difference = end-start;
+        System.out.println("Element für Element: " + difference + "ms");
+
+        Long start2 = System.currentTimeMillis();
+        klon2 = liste.clone();
+        Long end2 = System.currentTimeMillis();
+        Long difference2 = end2-start2;
+        System.out.println("Mit clone(): " + difference2 + "ms");
+
+       /* List<Integer> asdf = new ArrayList<>();
+        for(int i = 0; i < liste.length; i++) {
+            asdf.add(liste[i]);
+        }
+
+        asdf.forEach(System.out::println);*/
+    }
+
 }
---
+++ 411/AB411-05/src/AB05Main.java
@@ -0,0 +1,71 @@
+public class AB05Main {
+
+    final static int DIM1 = 12;
+    final static int DIM2 = 12;
+    static boolean[][] welt = new boolean[DIM1][DIM2];
+    // liefert eine zufällig initialisierte Welt
+    public static void initWelt() {
+
+        for(int i = 1; i < DIM1-1; i++){
+            for(int j = 1; j < DIM2-1; j++){
+                welt[i][j] = Math.random() > 0.4;
+            }
+        }
+    }
+    // gibt die aktuelle Welt aus
+    public static void zeigeWelt(boolean[][] welt) {
+        for(int i = 1; i < DIM1-1; i++){
+            for(int j = 1; j < DIM2-1; j++){
+                if(welt[i][j]){
+                    System.out.print("\tX");
+                }else {
+                    System.out.print("\t-");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int countNeighbours(int x, int y){
+        int neighbours = 0;
+        for (int i=x-1; i<=x+1; ++i){
+            for (int j=y-1; j<=y+1; ++j){
+                if (welt[i][j]){
+                    neighbours++;
+                }
+            }
+        }
+        if(welt[x][y]){
+            neighbours--;
+        }
+        return neighbours;
+    }
+
+    public static void prepareNextGen(){
+        for(int i = 1; i < DIM1-1; i++){
+            for(int j = 1; j < DIM2-1; j++){
+                int neighbours = countNeighbours(i,j);
+                if(welt[i][j] && (neighbours < 2 || neighbours > 3)){
+                    welt[i][j] = false;
+                }else if(!welt[i][j] && neighbours == 3){
+                    welt[i][j] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        initWelt();
+        System.out.println("Startkonstellation");
+        while(true){
+            zeigeWelt(welt);
+            System.out.println("\n\n");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            prepareNextGen();
+        }
+    }
+}
